d66fd441f41b3eba8ef70f950f36ab2bb3acdc7d
Troudi27 <troudi27troudi@gmail.com>
2024-06-27 23:52:39 +0100

    70% of routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import Navbar from "./components/navbar/Navbar"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from"./pages/home/Home"
+import Footer from "./components/footer/Footer";
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router =createBrowserRouter([
+    {
+      path:"/",
+      element:<Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>,
+        }
+      ]
+    }
+  ])
+
   return (
     <div>
-      <Navbar/>
+      <RouterProvider router={router}/>
     </div>
   )
 }
