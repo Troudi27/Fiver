a48ad75bd67ed2fc9fb52db1ba3b4279cd6f4e16
Troudi27 <troudi27troudi@gmail.com>
2024-07-02 21:59:14 +0100

    Completed UI TrustedBy

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Featured"
+import TrustedBy from "../../components/trustedBy/TrustedBy"
 import "./Home.scss"
 
 export default function Home() {
   return (
     <div>
       <Featured/>
+      <TrustedBy/>
     </div>
   )
 }
