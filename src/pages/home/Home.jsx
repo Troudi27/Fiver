471231f0620b89e0185a5a7bca40e707bdcdc596
Troudi27 <troudi27troudi@gmail.com>
2024-07-03 21:58:16 +0100

    Completed UI Slide

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import Featured from "../../components/featured/Featured"
+import Slide from "../../components/slide/Slide"
 import TrustedBy from "../../components/trustedBy/TrustedBy"
 import "./Home.scss"
+import {cards} from "./../../data"
+import CatCard from "../../components/catCard/CatCard"
 
 export default function Home() {
   return (
     <div>
       <Featured/>
       <TrustedBy/>
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map(card =>(
+          <CatCard item={card} key={card.id}/>
+        ))}
+      </Slide>
     </div>
   )
 }
