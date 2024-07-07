98b5591dc9968f67578644d050081e74bf46a14a
Troudi27 <troudi27troudi@gmail.com>
2024-07-08 00:16:37 +0100

    completed UI List of Gigs & Orders & messages

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -1,7 +1,52 @@
+import { Link } from "react-router-dom"
 import "./Messages.scss"
+
+const message = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis quia, alias cumque quas laudantium quibusdam exercitationem delectus provident explicabo quis ullam autem officiis reiciendis ducimus adipisci nesciunt assumenda architecto. Ex!`
 
 export default function Messages() {
   return (
-    <div>Messages</div>
+    <div className="messages">
+      <div className="container">
+        <div className="title">
+          <h1>messages</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Buyer</th>
+            <th>Last message</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+          <tr className="active">
+            <td>Houssem Troudi</td>
+            <td><Link to="/message/123" className="link">{message.substring(0,100)}...</Link></td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr className="active">
+            <td>Houssem Troudi</td>
+            <td><Link to="/message/123" className="link">{message.substring(0,100)}...</Link></td>
+            <td>1 day ago</td>
+            <td>
+              <button>Mark as Read</button>
+            </td>
+          </tr>
+
+          <tr >
+            <td>Houssem Troudi</td>
+            <td><Link to="/message/123" className="link">{message.substring(0,100)}...</Link></td>
+            <td>1 day ago</td>
+          </tr>
+          <tr >
+            <td>Houssem Troudi</td>
+            <td><Link to="/message/123" className="link">{message.substring(0,100)}...</Link></td>
+            <td>1 day ago</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,7 +1,65 @@
+
 import "./Orders.scss"
 
 export default function Orders() {
+
+  const curentUser ={
+    id:1,
+    username:"houssem",
+    isSeller:true,
+}
+
   return (
-    <div>Orders</div>
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{curentUser ?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="pimg" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>122</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img className="pimg" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>122</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <img className="pimg" src="https://images.pexels.com/photos/11295165/pexels-photo-11295165.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+            </td>
+            <td>Gig 1</td>
+            <td>88</td>
+            <td>122</td>
+            <td>
+              <img className="message" src="/img/message.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
