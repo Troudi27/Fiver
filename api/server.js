fdd9f106c4c19601e2d7085ea5bbc9a3aa90a873
Troudi27 <troudi27troudi@gmail.com>
2024-07-08 22:36:02 +0100

    Create an Express API files (controllers - Routes)

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import orderRoute from "./routes/order.route.js";
+import gigRoute from "./routes/gig.route.js";
+import reviewRoute from "./routes/review.route.js";
+import conversationRoute from "./routes/conversation.route.js";
 
 const app =express();
 dotenv.config();
@@ -17,6 +23,13 @@ const connect = async()=>{
 // ========================    
 };
 
+
+app.use("/api/users",userRoute);
+app.use("/api/gigs",gigRoute);
+app.use("/api/orders",orderRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
+app.use("/api/reviews",reviewRoute);
 
 app.listen(8800 , ()=>{
     connect()
