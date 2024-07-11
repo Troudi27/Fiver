884fe8e7944de708577ed34ba6aee4d272c7db1d
Troudi27 <troudi27troudi@gmail.com>
2024-07-11 16:59:19 +0100

    Logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,5 +46,11 @@ export const login = async (req, res, next) => {
 }
 
 export const logout = async (req, res) => {
-    res.send("logoutmmmmmmmmmmmm");
-}
+
+      res.clearCookie("accessToken", {
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .send("User has been logged out.");
+  };
