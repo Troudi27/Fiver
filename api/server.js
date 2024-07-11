477144efdc4a798ecff13e1a47f69f368a34949a
Troudi27 <troudi27troudi@gmail.com>
2024-07-11 23:49:29 +0100

    link login backend

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
@@ -26,6 +27,7 @@ const connect = async()=>{
 // ========================    
 };
 
+app.use(cors({origin:"http://localhost:5173", credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import "./Login.scss";
+import axios from "axios";
 
 function Login() {
+
+const[username,setUsername] = useState("")
+const[password,setPassword] = useState("")
+const[error,setError] = useState(null)
+
+const handelSubmit = async (e)=>{
+  e.preventDefault()
+
+try {
+  const res = await axios.post("http://localhost:8800/api/auth/login",{
+    username , 
+    password,
+  });
+  console.log(res.data)
+} catch (err) {
+  setError(err.data);
+  console.log(err)
+}
+};
+
+
+
   return (
     <div className="login">
-      <form >
+      <form onSubmit={handelSubmit}>
         <h1>Sign in</h1>
         <label htmlFor="">Username</label>
-        <input name="username" type="text" placeholder="Troudi27"/>
+        <input name="username" type="text" placeholder="Troudi27" onChange={e=>setUsername(e.target.value)}/>
         <label htmlFor="">Password</label>
-        <input name="password" type="password" />
+        <input name="password" type="password" onChange={e=>setPassword(e.target.value)}/>
         <button type="submit">Login</button>
       </form>
     </div>
