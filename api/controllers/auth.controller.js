8a9ec8e684068e5584af3483780c3ad9200b4f1a
Troudi27 <troudi27troudi@gmail.com>
2024-07-12 22:04:49 +0100

    compleate login linking with navabar and logout

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
    try {
     const user = await User.findOne({username: req.body.username});
-    if(!user) return  next(createError(404,"user xxx"))
+    if(!user) return  next(createError(404,"user note found "))
 
     const isCorrect = bcrypt.compareSync(req.body.password , user.password);
     if(!isCorrect) return next(createError(404,"wrong password or username"))
