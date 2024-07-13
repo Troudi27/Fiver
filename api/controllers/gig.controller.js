040423e7ab4fc73043be3c266b0d602ed28f62ae
Troudi27 <troudi27troudi@gmail.com>
2024-07-13 23:50:15 +0100

    create Gig

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,3 +1,31 @@
-// export const fn = (req,res) =>{
-//    //TODO
-// }
+import Gig from "../models/gig.model.js"
+import createError from "../utils/createError.js"
+
+
+export const createGig = async(req,res, next) =>{
+    console.log('Controller - isSeller:', req.username);  // Debug: Check isSeller value
+    console.log('Controller - userId:', req.userId);      // Debug: Check userId value
+
+//    if(!req.isSeller) return next(createError(403 ,"only sellers can create a gig"))
+    const  newGig = new Gig({
+        userId:req.userId,
+        ...req.body
+    })
+
+    try {
+        const savedGig = await newGig.save()
+        res.status(201).json(savedGig)
+    } catch (error) {
+        next(err)
+    }
+}
+export const deleteGig = async(req,res, next) =>{
+   //TODO
+}
+export const getGig = async(req,res, next) =>{
+   //TODO
+}
+export const getGigs = async(req,res, next) =>{
+   //TODO
+}
+
